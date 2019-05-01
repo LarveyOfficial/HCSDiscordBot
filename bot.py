856a0e0790521a40f92e9f159049b41f33a21906
Larvey <luisrvervaet@gmail.com>
2019-05-01 11:46:18 -0400

    added stop command to start testing

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-import discord, datetime
+import discord
 
 bot = commands.Bot(command_prefix='$', case_insensitive=True)
 prefix = "$"
@@ -8,5 +8,15 @@ TOKEN = 'NTY1MzUzMzU1Njc4MDU2NDQ4.XMmJcQ.wnBjUGE_AT9NjxJ-QWVbQbRcQTE'
 bot.remove_command('help')
 print("Loading....")
 owner_ids=[245653078794174465]
+
+@bot.command()
+async def stop(ctx):
+    if ctx.author.id in [245653078794174465]:
+        await ctx.send(":wave:")
+        await bot.change_presence(status='offline')
+        await bot.logout()
+
+
+
 
 bot.run(TOKEN)
