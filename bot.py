34bea0db2bb2c5c54d2b2bf35db5a1bc6911dd87
Larvey <luisrvervaet@gmail.com>
2019-05-01 07:57:10 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,17 +4,9 @@ import discord, datetime
 bot = commands.Bot(command_prefix='$', case_insensitive=True)
 prefix = "$"
 version = "0.0.1"
-TOKEN = ''
+TOKEN = 'NTY1MzUzMzU1Njc4MDU2NDQ4.XMmJcQ.wnBjUGE_AT9NjxJ-QWVbQbRcQTE'
 bot.remove_command('help')
 print("Loading....")
 owner_ids=[245653078794174465]
-
-@bot.command()
-async def stop(ctx):
-    if ctx.author.id in [245653078794174465]:
-        await ctx.send(":wave:")
-        await bot.change_presence(status='offline')
-        await bot.logout()
-
 
 bot.run(TOKEN)
