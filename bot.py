ccf2bb1c198dcdb560c0d9d06eab56b4e6d4378c
Larvey <luisrvervaet@gmail.com>
2019-05-01 07:49:48 -0400

    Did stuff

--- bot.py
+++ bot.py
@@ -1,16 +1,20 @@
 from discord.ext import commands
-import pymongo, praw, re, time, os, datetime, asyncio, discord, time, random, requests
+import discord, datetime
 
+bot = commands.Bot(command_prefix='$', case_insensitive=True)
+prefix = "$"
 version = "0.0.1"
-bot = commands.bot(command_prefix='$', case_insensitive='true')
-prefix = '$'
 TOKEN = ''
-print("Hello World")
-
-@bot.event
-async def on_guild_join(guild):
-    make_file(guild)
+bot.remove_command('help')
+print("Loading....")
+owner_ids=[245653078794174465]
 
 @bot.command()
-async def test(ctx):
-    await ctx.send("Ok")
+async def stop(ctx):
+    if ctx.author.id in [245653078794174465]:
+        await ctx.send(":wave:")
+        await bot.change_presence(status='offline')
+        await bot.logout()
+
+
+bot.run(TOKEN)
