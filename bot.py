42d8894ad7e11235c611ab0211e801f4894bb609
Luis Vervaet <luisrvervaet@gmail.com>
2019-11-16 21:40:32 -0500

    Better Status

--- bot.py
+++ bot.py
@@ -750,6 +750,8 @@ async def get_weather():
     description = data['weather'][0]['main']
     icon = data['weather'][0]['icon']
 
+    time = " "
+
     if str(icon) == '01d':
         icon = ':sunny:'
     elif str(icon) == '02d':
@@ -769,27 +771,64 @@ async def get_weather():
     elif str(icon) == '50d':
         icon = ':fog:'
     elif str(icon) == '01n':
-        icon = ':last_quarter_moon_with_face: '
+        icon = ':last_quarter_moon_with_face:'
+        time = " Night "
     elif str(icon) == '02n':
         icon = ':last_quarter_moon_with_face:  :cloud:'
+        time = " Night "
     elif str(icon) == '10n':
         icon = ':cloud_rain:'
+        time = " Night "
     elif str(icon) == '03n':
         icon = ':white_sun_cloud:'
+        time = " Night "
     elif str(icon) == '04n':
         icon = ':cloud:'
+        time = " Night "
     elif str(icon) == '09n':
         icon = ':cloud_rain:'
+        time = " Night "
     elif str(icon) == '11n':
         icon = ':thunder_cloud_rain:'
+        time = " Night "
     elif str(icon) == '13n':
         icon = ':cloud_snow:'
+        time = " Night "
     elif str(icon) == '50n':
         icon = ':fog:'
+        time = " Night "
     if str(description) == "Clear" and icon == ":sunny:":
         description = "Sunny"
+    elif str(description) == "Thunderstorm":
+        description = "Thunderstorming"
+    elif str(description) == "Drizzle":
+        description = "Drizzling"
+    elif str(description) == "Rain":
+        description = "Rainy"
+    elif str(description) == "Snow":
+        description = "Snowy"
+    elif str(description) == "Mist":
+        description = "Misty"
+    elif str(description) == "Smoke":
+        description = "Smoky"
+    elif str(description) == "Haze":
+        description = "Hazzy"
+    elif str(description) == "Dust":
+        description = "Dusty"
+    elif str(description) == "Fog":
+        description = "Foggy"
+    elif str(description) == "Sand":
+        description = "Sandy"
+    elif str(description) == "Ash":
+        description = "Ashy"
+    elif str(description) == "Squall":
+        description = "Squally?"
+    elif str(description) == "Tornado":
+        description = "Tornadoing"
+    elif str(description) == "Clouds":
+        description = "Cloudy"
 
-    new_status = "{} Skies".format(description)
+    new_status = "The {}{}Sky".format(description, time)
     return new_status
 
 
